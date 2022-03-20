59ffe360f6616f46aa9d1e302c0ab2ef1c916ab1
QueenTesa Fuggett <qfuggett@gmail.com>
2022-03-20 19:54:54 -0400

    test fetch request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  function getData() {
+    fetch('/main')
+    .then((response) => response.json())
+      .then((data) => {
+        setData(data)
+        console.log(data)
+    })
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +31,13 @@ function App() {
         >
           Learn React
         </a>
+
+        <p>Details</p>
+        <button onClick={getData} >Click</button>
+        {data && <div>
+          <p>Email: {data.email}</p>
+          <p>Name: {data.name}</p>
+        </div>}
       </header>
     </div>
   );
