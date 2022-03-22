f73c43d208f956b942f1b5ae71f5b27cbc02b250
QueenTesa Fuggett <qfuggett@gmail.com>
2022-03-22 16:47:55 -0400

    created delete crud action

--- backend/crud.py
+++ backend/crud.py
@@ -35,6 +35,14 @@ def get_user_by_email(email):
     return User.query.filter(User.email == email).first()
 
 
+def delete_user(user_id):
+    """Deletes a user by id"""
+
+    User.query.filter(User.user_id == user_id).delete()
+
+    return db.session.commit()
+
+
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
