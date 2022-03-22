da2d2761c155f44ee30d487fec24d02bef69be2f
QueenTesa Fuggett <qfuggett@gmail.com>
2022-03-22 16:50:53 -0400

    created deleteUser function

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -1,4 +1,29 @@
+import React, { useContext } from 'react';
+import { Button } from "react-bootstrap";
+import { UseUserContext } from "../App";
+
 export default function User(props) {
+
+    const { userData, setUserData } = useContext(UseUserContext);
+
+    function updateUser(e) {
+        e.preventDefault();
+        console.log("update user edit button clicked")
+        
+        
+    }
+
+    function deleteUser() {
+        console.log(props.id, props.name, props.email, props.zip)
+        const user_id = props.id
+
+        fetch(`/user/{ user_id }`, {
+            method: 'DELETE',
+            body: JSON.stringify({ 'user_id': props.id }),
+            headers: {'Content-type': 'application/json'}
+        })
+    }
+
     return (
         <tr>
             <th>{props.id}</th>  
@@ -6,6 +31,8 @@ export default function User(props) {
             <th>{props.email}</th>
             <th>{props.birthday}</th>
             <th>{props.zip}</th>
+            <th><Button onClick={updateUser}>Edit</Button></th>
+            <th><Button onClick={deleteUser}>Delete</Button></th>
         </tr>
     )
 }
