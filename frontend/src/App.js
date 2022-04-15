7492070fc668a191f1b489a79848a8809c6cec0e
QueenTesa Fuggett <qfuggett@gmail.com>
2022-04-15 11:38:22 -0400

    removed api fetch from app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,17 @@
-import React, { useState, useEffect, createContext } from 'react';
+import React from 'react';
 import './App.css';
-import User from './components/User';
 import UserForm from './components/UserForm';
 import UserTable from './components/table';
 
-export const UseUserContext = createContext({});
-
 function App() {
 
-  const [userData, setUserData] = useState(null);
-
-    useEffect(() => {
-        fetch('/allusers')
-        .then(response => response.json())
-        .then((data) => setUserData(data))
-    }, [])
-
-    const holdUser = []
-    for (let user in userData) {
-        holdUser.push(
-          <User
-            key={userData[user].user_id}
-            id={userData[user].user_id}
-            name={userData[user].name}
-            email={userData[user].email}
-            birthday={userData[user].birthday}
-            zip={userData[user].zip_code} />
-        )
-  };
-  
   return (
-    <UseUserContext.Provider className="App" value={{ userData }}>
+    <div>
       <header className="App-header">
         <UserForm />
-        <UserTable holdUser={holdUser}/>
+        <UserTable />
       </header>
-    </UseUserContext.Provider>
+    </div>
   );
 }
 
